4bea2f0e9d2afab57aa70f4ea66f6d37e83c44bc
masanori64 <23fi025@ms.dendai.ac.jp>
2025-06-13 13:46:58 +0900

    add omikuji

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from typing import Optional
-
 from fastapi import FastAPI
+import random  # randomライブラリを追加
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
@@ -12,3 +11,19 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/omikuji")
+def omikuji():
+    omikuji_list = [
+        "大吉",
+        "中吉",
+        "小吉",
+        "吉",
+        "半吉",
+        "末吉",
+        "末小吉",
+        "凶",
+        "小凶",
+        "大凶"
+    ]
+    return {"result": random.choice(omikuji_list)}
